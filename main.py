027ac4287f84270c06d717f3c9f3e3e577a8fca4
ksoeto <76516873+ksoeto@users.noreply.github.com>
2021-05-04 14:06:15 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# Imports
+import argparse
+import csv
+from datetime import date
+
+# Do not change these lines.
+__winc_id__ = 'a2bc36ea784242e4989deb157d527ba0'
+__human_name__ = 'superpy'
+
+
+# Your code below this line.
+def main():
+    parser = argparse.ArgumentParser(description='my supemarket')
+    parser.add_argument('product',metavar='product', type=str, help='find your product')
+    args = parser.parse_args()
+    product= args.product
+    buy = subparser.add_parser('buy', help='register bought products')
+    sell = subparser.add_parser('sell', help='register sold product')
+    report = subparser.add_parser('report', help='report transactions')
+
+def write_to_csv(dict):
+    with open('inventory.csv', 'w', newline='') as csvfile:
+        fieldnames = ['product_name', 'count', 'buy_price', 'expiration_date']
+        dict_writer = csv.DictWriter(csvfile, fieldnames=fieldnames, extrasaction='ignore')
+        
+
+d= datetime.date(2021, 5, 4)
+
+
+
+if __name__ == '__main__':
+    main()
